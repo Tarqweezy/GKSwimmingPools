8ef67a8e949c309179a0a3c2346fe29d953b9724
Tarqweezy <tarqweezy@gmail.com>
2026-07-05 22:42:15 -0700

    Delete gk-swimming-pools-redesigned directory

--- gk-swimming-pools-redesigned/gk-swimming-pools-redesigned/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://www.gkswimmingpools.com',
-  integrations: [
-    sitemap(),
-  ],
-});
